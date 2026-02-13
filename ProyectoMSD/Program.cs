614bb88d42da99535913d3c77c1b2c6ffcf2ea7b
MillerTC <miller.yara@todoscomemos.com>
2026-02-13 16:46:20 -0500

    demoparaportafolio

--- ProyectoMSD/Program.cs
+++ ProyectoMSD/Program.cs
@@ -5,7 +5,6 @@ using ProyectoMSD.Modelos;
 var builder = WebApplication.CreateBuilder(args);
 
 // Configurar para escuchar en todas las interfaces de red en el puerto 5000
-builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 builder.Services.AddRazorPages();
 
@@ -20,17 +19,69 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.Cookie.Name = "MySmartDeviceCookie";
     });
 
-// Configurar el DbContext con MySQL
 var connString = builder.Configuration.GetConnectionString("ConexionSQL");
-var serverVersion = ServerVersion.AutoDetect(connString);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(
-        connString,
-        serverVersion)
-);
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseInMemoryDatabase("DemoDatabase"));
+}
+else
+{
+    var serverVersion = ServerVersion.AutoDetect(connString);
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseMySql(connString, serverVersion));
+}
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (app.Environment.IsProduction())
+    {
+        context.Database.EnsureCreated();
+
+        if (!context.Usuarios.Any())
+        {
+            var adminUser = new Usuario
+            {
+                Nombre = "Administrador",
+                Clave = "123456",
+                Correo = "admin@demo.com",
+                Rol = "Admin",
+                Telefono = 300000000,
+                Ubicacion = "Demo",
+                Permisos = "Full",
+                Documento = 123456789,
+                Rut = "RUT-ADMIN",
+                Acesso = "Total"
+            };
+
+            var normalUser = new Usuario
+            {
+                Nombre = "Usuario Demo",
+                Clave = "123456",
+                Correo = "user@demo.com",
+                Rol = "User",
+                Telefono = 300000001,
+                Ubicacion = "Demo",
+                Permisos = "Limited",
+                Documento = 987654321,
+                Rut = "RUT-USER",
+                Acesso = "Parcial"
+            };
+
+            context.Usuarios.Add(adminUser);
+            context.Usuarios.Add(normalUser);
+
+            context.SaveChanges();
+        }
+    }
+}
+
 
 // Configuración del pipeline HTTP
 if (!app.Environment.IsDevelopment())
